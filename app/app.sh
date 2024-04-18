a21e92bbba4a26fd9c645852dab8142fc75fd926
Carlos Vivar Rios <45425937+caviri@users.noreply.github.com>
2024-04-18 15:39:17 +0200

    Fixing the symbolic link

--- app/app.sh
+++ app/app.sh
@@ -22,7 +22,8 @@ git checkout 86bf7bdc631961ac05c976fc280e78d93d666d02
 
 # A2B - Prepare datafolder
 
-ln -s /odtp/odtp-input/eqasim-matsim-output/simulation_output/${output_id}_legs.csv /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
+rm /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
+ln -s /odtp/odtp-input/eqasim-matsim-output/simulation_output/eqasim_legs.csv /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
 
 
 #########################################################
