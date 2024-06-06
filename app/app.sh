10a2f59369d4b1f0de8c5e4d4ab57b9a57ed9eac
sabinem <5292683+sabinem@users.noreply.github.com>
2024-06-06 08:00:50 +0200

    add DATA_INPUT_PATH as a parameter

--- app/app.sh
+++ app/app.sh
@@ -22,9 +22,7 @@ git checkout 86bf7bdc631961ac05c976fc280e78d93d666d02
 
 # A2B - Prepare datafolder
 
-rm /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
-ln -s /odtp/odtp-input/eqasim-matsim-output/simulation_output/eqasim_legs.csv /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
-
+ln -s $INPUT_DATA_PATH /odtp/odtp-workdir/OD_data_dashboard/data/origin-destination.csv
 
 #########################################################
 # COMMAND TO RUN THE TOOL
